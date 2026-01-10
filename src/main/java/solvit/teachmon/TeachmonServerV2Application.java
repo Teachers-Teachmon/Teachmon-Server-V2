b381567cb7d8d77120e98b9662edb2f2f9c18a59
Lian08 <lian080408@gmail.com>
2026-01-10 16:19:08 +0900

    Initial Commit

---
+++ src/main/java/solvit/teachmon/TeachmonServerV2Application.java
@@ -0,0 +1,13 @@
+package solvit.teachmon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachmonServerV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeachmonServerV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/solvit/teachmon/TeachmonServerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package solvit.teachmon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachmonServerV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
