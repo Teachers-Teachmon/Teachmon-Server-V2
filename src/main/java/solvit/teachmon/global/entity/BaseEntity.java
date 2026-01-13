c1579c6a59219390498f7d0574ba03eb6b64ea46
Lian08 <lian080408@gmail.com>
2026-01-13 18:35:05 +0900

    feat#3 :: BaseEntity 수정 및 설정 파일 업데이트

--- src/main/java/solvit/teachmon/global/entity/BaseEntity.java
+++ src/main/java/solvit/teachmon/global/entity/BaseEntity.java
@@ -4,7 +4,6 @@ import com.github.lian2945.sonyflake.annotation.SonyflakeId;
 import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
