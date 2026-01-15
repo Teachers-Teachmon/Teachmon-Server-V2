17f32b6375d149d10f5438fac26eff599d814aa9
Lian08 <lian080408@gmail.com>
2026-01-15 22:07:49 +0900

    feat#2 :: AuthCodeRepository 추가

--- src/main/java/solvit/teachmon/domain/auth/domain/repository/AuthCodeRepository.java
+++ src/main/java/solvit/teachmon/domain/auth/domain/repository/AuthCodeRepository.java
@@ -1,4 +1,9 @@
 package solvit.teachmon.domain.auth.domain.repository;
 
-public class AuthCodeRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import solvit.teachmon.domain.auth.domain.entity.AuthCodeEntity;
+
+@Repository
+public interface AuthCodeRepository extends CrudRepository<AuthCodeEntity, String> {
 }
