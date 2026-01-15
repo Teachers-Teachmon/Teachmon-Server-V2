7f73b27ae9c9ddf15c01b75701fca82b64c20529
Lian08 <lian080408@gmail.com>
2026-01-15 22:07:50 +0900

    feat#2 :: AuthCodeEntity 추가

--- src/main/java/solvit/teachmon/domain/auth/domain/entity/AuthCodeEntity.java
+++ src/main/java/solvit/teachmon/domain/auth/domain/entity/AuthCodeEntity.java
@@ -1,9 +1,26 @@
 package solvit.teachmon.domain.auth.domain.entity;
 
+import lombok.Builder;
 import lombok.Getter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
 
 @Getter
 @RedisHash("code")
 public class AuthCodeEntity {
+    @Id
+    private final String authCode;
+
+    private final String accessToken;
+
+    @TimeToLive
+    private final Long timeToLive;
+
+    @Builder
+    public AuthCodeEntity(String authCode, String accessToken, Long timeToLive) {
+        this.authCode = authCode;
+        this.accessToken = accessToken;
+        this.timeToLive = timeToLive;
+    }
 }
