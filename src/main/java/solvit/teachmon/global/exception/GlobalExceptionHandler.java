7fd9e1f6f6cf2e8171b6a1b839f0deb4ee470c1c
huhonmcmc <huhonmcmc@gmail.com>
2026-01-15 21:36:00 +0900

    feat#9 :: 일별 자습 추가 조회 api 수정

--- src/main/java/solvit/teachmon/global/exception/GlobalExceptionHandler.java
+++ src/main/java/solvit/teachmon/global/exception/GlobalExceptionHandler.java
@@ -2,11 +2,20 @@ package solvit.teachmon.global.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST.value())
+                .body(errorResponse);
+    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
