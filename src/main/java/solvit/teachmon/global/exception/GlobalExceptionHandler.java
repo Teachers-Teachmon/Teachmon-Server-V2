d10738c9c10eb5dd92c8a386ec475ea20b1eff74
Lian08 <lian080408@gmail.com>
2026-01-15 22:07:37 +0900

    feat#2 :: GlobalExceptionHandler Auth 예외 처리 추가

--- src/main/java/solvit/teachmon/global/exception/GlobalExceptionHandler.java
+++ src/main/java/solvit/teachmon/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package solvit.teachmon.global.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
