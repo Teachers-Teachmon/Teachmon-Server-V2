8d164bf26a7a9513e7d97aba12c5f8aa653323ca
Lian08 <lian080408@gmail.com>
2026-01-15 22:07:46 +0900

    feat#2 :: TeachmonOAuth2UserFacade 구현

--- src/main/java/solvit/teachmon/domain/auth/infrastructure/security/facade/TeachmonOAuth2UserFacade.java
+++ src/main/java/solvit/teachmon/domain/auth/infrastructure/security/facade/TeachmonOAuth2UserFacade.java
@@ -1,4 +1,4 @@
-package solvit.teachmon.domain.oauth2.infrastructure.security.facade;
+package solvit.teachmon.domain.auth.infrastructure.security.facade;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -6,10 +6,10 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-import solvit.teachmon.domain.oauth2.exception.UnsupportedAccountException;
-import solvit.teachmon.domain.oauth2.infrastructure.security.strategy.OAuth2StrategyComposite;
-import solvit.teachmon.domain.oauth2.infrastructure.security.vo.TeachmonOAuth2User;
-import solvit.teachmon.domain.oauth2.infrastructure.security.vo.TeachmonOAuth2UserInfo;
+import solvit.teachmon.domain.auth.exception.UnsupportedAccountException;
+import solvit.teachmon.domain.auth.infrastructure.security.strategy.OAuth2StrategyComposite;
+import solvit.teachmon.domain.auth.infrastructure.security.vo.TeachmonOAuth2User;
+import solvit.teachmon.domain.auth.infrastructure.security.vo.TeachmonOAuth2UserInfo;
 import solvit.teachmon.domain.user.application.service.TeacherAuthenticationService;
 import solvit.teachmon.domain.user.domain.enums.OAuth2Type;
 import solvit.teachmon.domain.user.domain.enums.Role;
@@ -23,7 +23,7 @@ public class TeachmonOAuth2UserFacade extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oauth2User = super.loadUser(userRequest);
-        OAuth2Type oAuth2Type = OAuth2Type.of(userRequest.getClientRegistration().getClientId());
+        OAuth2Type oAuth2Type = OAuth2Type.of(userRequest.getClientRegistration().getRegistrationId());
         TeachmonOAuth2UserInfo teachmonOAuth2UserInfo = oAuth2StrategyComposite.getOAuth2Strategy(oAuth2Type).getUserInfo(oauth2User);
         checkAccount(teachmonOAuth2UserInfo.mail());
         Role role = teacherAuthenticationService.getRole(teachmonOAuth2UserInfo);
