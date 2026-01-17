51ad963d9574b94590ab46d9e7fba47346f40adb
huhonmcmc <huhonmcmc@gmail.com>
2026-01-17 17:59:20 +0900

    feat#1 :: @Trace 삭제

--- src/main/java/solvit/teachmon/domain/management/student/application/service/ManagementStudentService.java
+++ src/main/java/solvit/teachmon/domain/management/student/application/service/ManagementStudentService.java
@@ -7,7 +7,6 @@ import solvit.teachmon.domain.management.student.domain.entity.StudentEntity;
 import solvit.teachmon.domain.management.student.domain.exception.StudentNotFoundException;
 import solvit.teachmon.domain.management.student.domain.repository.StudentRepository;
 import solvit.teachmon.domain.management.student.presentation.dto.request.StudentRequest;
-import solvit.teachmon.global.annotation.Trace;
 
 import java.time.LocalDate;
 
@@ -16,7 +15,6 @@ import java.time.LocalDate;
 public class ManagementStudentService {
     private final StudentRepository studentRepository;
 
-    @Trace
     @Transactional
     public void createStudent(StudentRequest request) {
         StudentEntity student = StudentEntity.builder()
@@ -30,7 +28,6 @@ public class ManagementStudentService {
         studentRepository.save(student);
     }
 
-    @Trace
     @Transactional
     public void updateStudent(Long studentId, StudentRequest request) {
         StudentEntity student = studentRepository.findById(studentId)
@@ -39,7 +36,6 @@ public class ManagementStudentService {
         student.changeInfo(request.grade(), request.classNumber(), request.number(), request.name());
     }
 
-    @Trace
     @Transactional
     public void deleteStudent(Long studentId) {
         if (!studentRepository.existsById(studentId)) {
