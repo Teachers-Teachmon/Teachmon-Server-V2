9bf959fb50e3771b90e870972952486152dd9d77
Lian08 <lian080408@gmail.com>
2026-01-21 23:57:09 +0900

    fix: TeamQueryDslRepository @Repository 어노테이션 제거
    - Spring Data JPA가 자동 쿼리 메서드 생성 시도하는 문제 해결
    - QueryDSL 인터페이스는 @Repository 불필요
    - Application context load 테스트 수정 필요

--- src/main/java/solvit/teachmon/domain/team/domain/repository/querydsl/TeamQueryDslRepository.java
+++ src/main/java/solvit/teachmon/domain/team/domain/repository/querydsl/TeamQueryDslRepository.java
@@ -1,11 +1,9 @@
 package solvit.teachmon.domain.team.domain.repository.querydsl;
 
-import org.springframework.stereotype.Repository;
 import solvit.teachmon.domain.team.presentation.dto.response.TeamResponseDto;
 
 import java.util.List;
 
-@Repository
 public interface TeamQueryDslRepository {
     List<TeamResponseDto> searchTeamByQuery(String query);
 }
