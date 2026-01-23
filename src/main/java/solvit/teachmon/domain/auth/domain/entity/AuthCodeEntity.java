f99c85a1d4aef8bd761a7250e08eef20572103c7
Lian08 <lian080408@gmail.com>
2026-01-23 12:18:18 +0900

    feat#2 :: AuthCodeEntity 수정

--- src/main/java/solvit/teachmon/domain/auth/domain/entity/AuthCodeEntity.java
+++ src/main/java/solvit/teachmon/domain/auth/domain/entity/AuthCodeEntity.java
@@ -20,19 +20,27 @@ public class AuthCodeEntity {
 
     @Builder
     public AuthCodeEntity(String authCode, String accessToken, Long timeToLive) {
-        validateFields(authCode, accessToken, timeToLive);
+        validateAuthCodeField(authCode);
+        validateAccessTokenField(accessToken);
+        validateTimeToLiveField(timeToLive);
 
         this.authCode = authCode;
         this.accessToken = accessToken;
         this.timeToLive = timeToLive;
     }
 
-    private void validateFields(String authCode, String accessToken, Long timeToLive) {
+    private void validateAuthCodeField(String authCode) {
         if(authCode == null || authCode.trim().isEmpty())
             throw new InvalidAuthCodeInfoException("인증 코드는 비어 있을 수 없습니다.");
+    }
+
+    private void validateAccessTokenField(String accessToken) {
         if(accessToken == null || accessToken.trim().isEmpty())
             throw new InvalidAuthCodeInfoException("액세스 토큰은 비어 있을 수 없습니다.");
-        if(timeToLive == null || timeToLive < 0)
+    }
+
+    private void validateTimeToLiveField(Long timeToLive) {
+        if(timeToLive == null ||timeToLive < 0)
             throw new InvalidAuthCodeInfoException("TTL 값은 0 이상이어야 합니다.");
     }
 }
