71cc32ffa186a5f48b39118a9ad1da657e1c0b3f
Lian08 <lian080408@gmail.com>
2026-01-26 10:39:00 +0900

    feat#2 :: nameField -> name 수정

--- src/main/java/solvit/teachmon/domain/user/domain/entity/TeacherEntity.java
+++ src/main/java/solvit/teachmon/domain/user/domain/entity/TeacherEntity.java
@@ -46,7 +46,7 @@ public class TeacherEntity extends BaseEntity {
         validateMail(mail);
         validateProviderId(providerId);
         validateOAuth2Type(oAuth2Type);
-        validateNameFiled(name);
+        validateName(name);
 
         this.mail = mail;
         this.name = name;
@@ -72,7 +72,7 @@ public class TeacherEntity extends BaseEntity {
             throw new InvalidTeacherInfoException("OAuth2 타입은 비어 있을 수 없습니다.");
     }
 
-    private void validateNameFiled(String name) {
+    private void validateName(String name) {
         if(name == null || name.trim().isEmpty())
             throw new InvalidTeacherInfoException("이름은 비어 있을 수 없습니다.");
     }
