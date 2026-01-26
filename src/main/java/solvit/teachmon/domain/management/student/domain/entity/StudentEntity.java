99ec9921a4a14eac0c37bceeb1325a169d3763ad
Lian08 <lian080408@gmail.com>
2026-01-26 11:18:38 +0900

    feat#2 :: StudentEntity 수정

--- src/main/java/solvit/teachmon/domain/management/student/domain/entity/StudentEntity.java
+++ src/main/java/solvit/teachmon/domain/management/student/domain/entity/StudentEntity.java
@@ -17,7 +17,7 @@ import java.time.LocalDate;
 @Table(name = "student")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StudentEntity extends BaseEntity {
-    @Column(name = "`year`", nullable = false)
+    @Column(name = "year", nullable = false)
     private Integer year;
 
     @Column(name = "grade", nullable = false)
@@ -32,36 +32,22 @@ public class StudentEntity extends BaseEntity {
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Builder(builderMethodName = "withYearBuilder")
+    @Builder
     public StudentEntity(Integer year, Integer grade, Integer classNumber, Integer number, String name) {
-        validateYear(year);
-        validateGrade(grade);
-        validateClassNumber(classNumber);
-        validateNumber(number);
-        validateName(name);
-
-        this.year = year;
+        this.year = resolveYear(year);
         this.grade = grade;
         this.classNumber = classNumber;
         this.number = number;
         this.name = name;
     }
 
-    @Builder(builderMethodName = "withCurrentYearBuilder")
-    public StudentEntity(Integer grade, Integer classNumber, Integer number, String name) {
-        validateGrade(grade);
-        validateClassNumber(classNumber);
-        validateNumber(number);
-        validateName(name);
-
-        this.year = getNowYear();
-        this.grade = grade;
-        this.classNumber = classNumber;
-        this.number = number;
-        this.name = name;
+    private Integer resolveYear(Integer year) {
+        // year 이 유효한지 검사
+        return (year != null) ? year : getNowYear();
     }
 
     private Integer getNowYear() {
+        // year 설정 도메인 로직
         return LocalDate.now().getYear();
     }
 
@@ -77,32 +63,27 @@ public class StudentEntity extends BaseEntity {
         this.name = name;
     }
 
-    private void validateYear(Integer year) {
-        if(year == null || year < 0)
-            throw new InvalidStudentInfoException("year(연도)는 1 이상이어야 합니다.");
-    }
-
     private void validateGrade(Integer grade) {
         if (grade == null || grade < 1 || grade > 3) {
-            throw new InvalidStudentInfoException("grade(학년)은 1 ~ 3 사이여야 합니다.");
+            throw new InvalidStudentInfoException("학년은 1 ~ 3 사이여야 합니다");
         }
     }
 
     private void validateClassNumber(Integer classNumber) {
         if (classNumber == null || classNumber < 1) {
-            throw new InvalidStudentInfoException("classNumber(반)은 1 이상이어야 합니다.");
+            throw new InvalidStudentInfoException("반은 1 이상이어야 합니다");
         }
     }
 
     private void validateNumber(Integer number) {
         if (number == null || number < 1) {
-            throw new InvalidStudentInfoException("number(번호)는 1 이상이어야 합니다.");
+            throw new InvalidStudentInfoException("번호는 1 이상이어야 합니다");
         }
     }
 
     private void validateName(String name) {
         if (name == null || name.trim().isEmpty()) {
-            throw new InvalidStudentInfoException("name(이름)은 필수입니다.");
+            throw new InvalidStudentInfoException("이름은 비어 있을 수 없습니다");
         }
     }
 }
