69659a44d42ce8f48020d8399d235181f68ece0b
Lian08 <lian080408@gmail.com>
2026-02-05 19:35:53 +0900

    feat#33 :: base64 encoding 방식으로 변경

--- src/main/java/solvit/teachmon/global/configuration/GoogleSpreadSheetConfiguration.java
+++ src/main/java/solvit/teachmon/global/configuration/GoogleSpreadSheetConfiguration.java
@@ -5,6 +5,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
+import java.util.Base64;
 import java.util.Collections;
 
 @Configuration
@@ -26,10 +28,12 @@ public class GoogleSpreadSheetConfiguration {
     @Bean
     public Credential googleCredential() throws IOException {
         try {
-            String credentialsJson = googleSpreadSheetProperties.getCredentials();
-            ByteArrayInputStream credentialsStream = new ByteArrayInputStream(credentialsJson.getBytes(StandardCharsets.UTF_8));
-            return GoogleCredential.fromStream(credentialsStream)
-                    .createScoped(Collections.singletonList(googleSpreadSheetProperties.getCredentialsScope()));
+            byte[] decodedBytes = Base64.getDecoder().decode(googleSpreadSheetProperties.getCredentials());
+            String credentialsJson = new String(decodedBytes, StandardCharsets.UTF_8);
+
+            return GoogleCredential.fromStream(
+                    new ByteArrayInputStream(credentialsJson.getBytes(StandardCharsets.UTF_8))
+            ).createScoped(Collections.singletonList(SheetsScopes.SPREADSHEETS));
         }
         catch (IOException e) {
             throw new IOException("Google 서비스 계정 키 설정을 읽을 수 없습니다.", e);
