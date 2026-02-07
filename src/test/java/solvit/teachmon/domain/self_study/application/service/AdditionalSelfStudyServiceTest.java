75e26410511c067314388f2e66faddadd4b9f531
huhonmcmc <huhonmcmc@gmail.com>
2026-02-07 15:39:44 +0900

    feat#55 :: warning 제거

--- src/test/java/solvit/teachmon/domain/self_study/application/service/AdditionalSelfStudyServiceTest.java
+++ src/test/java/solvit/teachmon/domain/self_study/application/service/AdditionalSelfStudyServiceTest.java
@@ -3,7 +3,6 @@ package solvit.teachmon.domain.self_study.application.service;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -31,10 +30,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
