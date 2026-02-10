32310bd3bd42c5762450d09f65cc3e87e21b40b3
huhonmcmc <huhonmcmc@gmail.com>
2026-02-10 14:06:45 +0900

    feat#64 :: 주간 student_schedule 설정 api 반환 값 추가

--- src/main/java/solvit/teachmon/domain/student_schedule/presentation/controller/StudentScheduleSettingController.java
+++ src/main/java/solvit/teachmon/domain/student_schedule/presentation/controller/StudentScheduleSettingController.java
@@ -2,6 +2,7 @@ package solvit.teachmon.domain.student_schedule.presentation.controller;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +21,15 @@ public class StudentScheduleSettingController {
     private final StudentScheduleSettingService studentScheduleSettingService;
 
     @PostMapping("/weekly")
-    public void settingWeeklyStudentSchedule(
+    public ResponseEntity<String> settingWeeklyStudentSchedule(
             @RequestParam("base_day") @NotNull(message = "주간 학생 스케줄 설정시 base_day(기준 날)는 필수입니다.") LocalDate baseDay
     ) {
         baseDay = baseDay.with(DayOfWeek.MONDAY);
         studentScheduleSettingService.createNewStudentSchedule(baseDay);
         studentScheduleSettingService.settingAllTypeSchedule(baseDay);
+
+        return ResponseEntity
+                .ok()
+                .body("주간 학생 스케줄 설정이 완료되었습니다.");
     }
 }
