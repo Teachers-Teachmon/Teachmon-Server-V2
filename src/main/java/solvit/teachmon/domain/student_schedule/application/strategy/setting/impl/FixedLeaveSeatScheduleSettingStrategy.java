a2adfd4dcebff041edd842f69a6f7653215bd9b8
huhonmcmc <huhonmcmc@gmail.com>
2026-02-12 16:46:17 +0900

    refactor#83 :: 고정 이석에서 이석 생성만 하도록 수정

--- src/main/java/solvit/teachmon/domain/student_schedule/application/strategy/setting/impl/FixedLeaveSeatScheduleSettingStrategy.java
+++ src/main/java/solvit/teachmon/domain/student_schedule/application/strategy/setting/impl/FixedLeaveSeatScheduleSettingStrategy.java
@@ -10,13 +10,7 @@ import solvit.teachmon.domain.leave_seat.domain.repository.FixedLeaveSeatStudent
 import solvit.teachmon.domain.leave_seat.domain.repository.LeaveSeatRepository;
 import solvit.teachmon.domain.management.student.domain.entity.StudentEntity;
 import solvit.teachmon.domain.student_schedule.application.strategy.setting.StudentScheduleSettingStrategy;
-import solvit.teachmon.domain.student_schedule.domain.entity.ScheduleEntity;
-import solvit.teachmon.domain.student_schedule.domain.entity.StudentScheduleEntity;
-import solvit.teachmon.domain.student_schedule.domain.entity.schedules.LeaveSeatScheduleEntity;
 import solvit.teachmon.domain.student_schedule.domain.enums.ScheduleType;
-import solvit.teachmon.domain.student_schedule.domain.repository.ScheduleRepository;
-import solvit.teachmon.domain.student_schedule.domain.repository.StudentScheduleRepository;
-import solvit.teachmon.domain.student_schedule.domain.repository.schedules.LeaveSeatScheduleRepository;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -24,10 +18,7 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class FixedLeaveSeatScheduleSettingStrategy implements StudentScheduleSettingStrategy {
-    private final ScheduleRepository scheduleRepository;
-    private final StudentScheduleRepository studentScheduleRepository;
     private final LeaveSeatRepository leaveSeatRepository;
-    private final LeaveSeatScheduleRepository leaveSeatScheduleRepository;
     private final FixedLeaveSeatRepository fixedLeaveSeatRepository;
     private final FixedLeaveSeatStudentRepository fixedLeaveSeatStudentRepository;
 
@@ -40,11 +31,16 @@ public class FixedLeaveSeatScheduleSettingStrategy implements StudentScheduleSet
     public void settingSchedule(LocalDate baseDate) {
         List<FixedLeaveSeatEntity> fixedLeaveSeats = fixedLeaveSeatRepository.findAll();
 
-        for(FixedLeaveSeatEntity fixedLeaveSeat : fixedLeaveSeats) {
-            if(isBeforeLeaveSeat(fixedLeaveSeat, baseDate))
+        for (FixedLeaveSeatEntity fixedLeaveSeat : fixedLeaveSeats) {
+            LocalDate leaveSeatDay = calculateFixedLeaveSeatDay(fixedLeaveSeat, baseDate);
+
+            if (isBeforeLeaveSeat(fixedLeaveSeat, baseDate))
                 continue;
-            List<StudentScheduleEntity> studentSchedules = findStudentScheduleByFixedLeaveSeat(fixedLeaveSeat, baseDate);
-            settingLeaveSeatSchedule(studentSchedules, fixedLeaveSeat, baseDate);
+
+            if (isAlreadyCreated(fixedLeaveSeat, leaveSeatDay))
+                continue;
+
+            createLeaveSeat(fixedLeaveSeat, leaveSeatDay);
         }
     }
 
@@ -53,62 +49,28 @@ public class FixedLeaveSeatScheduleSettingStrategy implements StudentScheduleSet
         return fixedLeaveSeatDay.isBefore(baseDate);
     }
 
-    private List<StudentScheduleEntity> findStudentScheduleByFixedLeaveSeat(FixedLeaveSeatEntity fixedLeaveSeat, LocalDate baseDate) {
-        return studentScheduleRepository.findAllByFixedLeaveSeatAndDay(
-                fixedLeaveSeat,
-                calculateFixedLeaveSeatDay(fixedLeaveSeat, baseDate),
-                fixedLeaveSeat.getPeriod()
-        );
-    }
-
     private LocalDate calculateFixedLeaveSeatDay(FixedLeaveSeatEntity fixedLeaveSeat, LocalDate baseDate) {
         return baseDate.with(fixedLeaveSeat.getWeekDay().toDayOfWeek());
     }
 
-    private void settingLeaveSeatSchedule(
-            List<StudentScheduleEntity> studentSchedules,
-            FixedLeaveSeatEntity fixedLeaveSeat,
-            LocalDate baseDate
-    ) {
-        LeaveSeatEntity leaveSeat = createLeaveSeat(fixedLeaveSeat, baseDate);
-
-        for(StudentScheduleEntity studentSchedule : studentSchedules) {
-            ScheduleEntity newSchedule = createNewSchedule(studentSchedule);
-            createLeaveSeatSchedule(newSchedule, leaveSeat);
-        }
+    private boolean isAlreadyCreated(FixedLeaveSeatEntity fixedLeaveSeat, LocalDate leaveSeatDay) {
+        return leaveSeatRepository
+                .findByPlaceAndDayAndPeriod(fixedLeaveSeat.getPlace(), leaveSeatDay, fixedLeaveSeat.getPeriod())
+                .isPresent();
     }
 
-    private ScheduleEntity createNewSchedule(StudentScheduleEntity studentSchedule) {
-        Integer lastStackOrder = scheduleRepository.findLastStackOrderByStudentScheduleId(studentSchedule.getId());
-        ScheduleEntity newSchedule = ScheduleEntity.createNewStudentSchedule(studentSchedule, lastStackOrder, ScheduleType.FIXED_LEAVE_SEAT);
-        scheduleRepository.save(newSchedule);
-        return newSchedule;
-    }
+    private void createLeaveSeat(FixedLeaveSeatEntity fixedLeaveSeat, LocalDate leaveSeatDay) {
+        LeaveSeatEntity leaveSeat = LeaveSeatEntity.from(fixedLeaveSeat, leaveSeatDay);
 
-    private LeaveSeatEntity createLeaveSeat(FixedLeaveSeatEntity fixedLeaveSeat, LocalDate baseDate) {
-        LeaveSeatEntity leaveSeat = LeaveSeatEntity.from(fixedLeaveSeat, calculateFixedLeaveSeatDay(fixedLeaveSeat, baseDate));
-
-        List<StudentEntity> leaveSeatStudents = fixedLeaveSeatStudentRepository.findAllByFixedLeaveSeat(fixedLeaveSeat);
-        leaveSeatStudents
-                .forEach(student -> {
-                    LeaveSeatStudentEntity leaveSeatStudent = LeaveSeatStudentEntity.builder()
-                            .leaveSeat(leaveSeat)
-                            .student(student)
-                            .build();
-
-                    leaveSeat.addLeaveSeatStudent(leaveSeatStudent);
-                });
+        List<StudentEntity> students = fixedLeaveSeatStudentRepository.findAllByFixedLeaveSeat(fixedLeaveSeat);
+        students.forEach(student -> {
+            LeaveSeatStudentEntity leaveSeatStudent = LeaveSeatStudentEntity.builder()
+                    .leaveSeat(leaveSeat)
+                    .student(student)
+                    .build();
+            leaveSeat.addLeaveSeatStudent(leaveSeatStudent);
+        });
 
         leaveSeatRepository.save(leaveSeat);
-        return leaveSeat;
-    }
-
-    private void createLeaveSeatSchedule(ScheduleEntity schedule, LeaveSeatEntity leaveSeat) {
-        LeaveSeatScheduleEntity leaveSeatSchedule = LeaveSeatScheduleEntity.builder()
-                .schedule(schedule)
-                .leaveSeat(leaveSeat)
-                .build();
-
-        leaveSeatScheduleRepository.save(leaveSeatSchedule);
     }
 }
