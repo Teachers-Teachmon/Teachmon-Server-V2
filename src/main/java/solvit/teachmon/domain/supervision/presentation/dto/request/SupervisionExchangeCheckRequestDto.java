1546893f1085d1113694fd598b59b15efe98d1fb
Lian08 <lian080408@gmail.com>
2026-02-13 15:46:29 +0900

    refactor#79 :: GlobalExceptionHandler 방어적 코드 수정 및 No Static Error 고치기 & ChangeRequestDto Add

---
+++ src/main/java/solvit/teachmon/domain/supervision/presentation/dto/request/SupervisionExchangeCheckRequestDto.java
@@ -0,0 +1,13 @@
+package solvit.teachmon.domain.supervision.presentation.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+
+@Builder
+public record SupervisionExchangeCheckRequestDto(
+        @JsonProperty("exchange_request_id")
+        @NotNull(message = "교체 요청 ID는 필수입니다.")
+        Long exchangeRequestId
+) {
+}
